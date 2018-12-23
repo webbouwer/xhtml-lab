321df25941739d12fab8c413f41f3fa30fe0c287
Webbouwer <support@webdesigndenhaag.net>
2018-12-23 23:09:23 +0100

    scroll testing

---
+++ swiperjs/readme.md
@@ -0,0 +1,8 @@
+### Swiper JS setup
+
+Test with scroll/swipe inner content
+
+![event - onscroll - panning](https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/Art/events_panning.jpg)
+
+
+![event - scroll 2 finger ](https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/Art/events_panning.jpghttps://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/Art/events_2_finger_scroll.jpg)
--- swiperjs/swiper.js
+++ swiperjs/swiper.js
@@ -34,7 +34,6 @@ var swiper_page_vertical = new Swiper('.swiper-container', {
 });
 
 
-
 /* on resize */
 var resizeId;
 $(window).resize(function() {
@@ -46,5 +45,84 @@ function doneResizing() {
   swiper_page_vertical.update();
   swiper_page_vertical.scrollbar.updateSize();
 }
+
+
+/* scrolling .. */
+var dir = 1;
+var boxscroll = 1;
+var scrollamount = 1;
+disable_swiper();
+$('.swiper-slide').scrollTop(1);
+
+
+
+
+$(window).on('touchstart scroll wheel DOMMouseScroll', function(e){
+
+    var prevscroll = boxscroll;
+    var activeSlide = $('.swiper-slide-active');
+    var viewheight = activeSlide.find('.swiper-slide-wrap').height();
+    var scrollheight = activeSlide.find('.swiper-slide-content').height();
+
+    if( viewheight < scrollheight ){
+
+    	disable_swiper();
+
+            var scrollamount = activeSlide.find('.swiper-slide-wrap').scrollTop();
+            if( activeSlide.find('.swiper-slide-wrap').pageYOffset ){
+                scrollamount = activeSlide.find('.swiper-slide-wrap').pageYOffset;
+            }
+
+            if( scrollamount < prevscroll || scrollamount == 0 ){
+                dir = -1;
+            }else{
+                dir = 1;
+            }
+
+
+   					if( dir == -1 && prevscroll != 1 && scrollamount <= 0 ){
+            	boxscroll = 1;
+              dir = -1;
+              activeSlide.find('.swiper-slide-wrap').scrollTop(1);
+              swiper_page_vertical.slidePrev(swiper_page_vertical.speed);
+              //enable_swiper();
+            }
+            if( dir == 1 && scrollamount >= (scrollheight-viewheight) ){
+            	boxscroll = 1;
+              dir = -1;
+              activeSlide.find('.swiper-slide-wrap').scrollTop(1);
+              swiper_page_vertical.slideNext(swiper_page_vertical.speed);
+              //enable_swiper();
+            }
+      			boxscroll = scrollamount;
+
+            $('.swiper-event-info').fadeIn(400);
+            $('.swiper-event-info').html( scrollamount +' .. ' + dir + ' to '+ (scrollheight-viewheight) );
+
+    }else{
+            enable_swiper();
+            boxscroll = 1;
+            $('.swiper-event-info').fadeOut(400);
+    }
+
+
+
+});
+swiper_page_vertical.on('transitionStart', function () {
+    $('.swiper-slide').find('.swiper-slide-wrap').animate({ 'scrollTop': 1 }, swiper_page_vertical.speed );
+});
+swiper_page_vertical.on('slideNextTransitionEnd', function () {
+});
+function disable_swiper(){
+    swiper_page_vertical.detachEvents();
+    swiper_page_vertical.mousewheel.disable();
+    //swiper_page_vertical.unsetGrabCursor();
+}
+function enable_swiper(){
+    swiper_page_vertical.attachEvents();
+    swiper_page_vertical.mousewheel.enable();
+    //swiper_page_vertical.setGrabCursor();
+}
+
 
 });
